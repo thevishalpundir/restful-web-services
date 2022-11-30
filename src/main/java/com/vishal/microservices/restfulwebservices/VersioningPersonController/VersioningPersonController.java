8657ab40609144196bada9c3f8e333f6a41efecd
Vishal Pundir <vishal.pundir.ext@ocr-inc.com>
2022-11-30 18:10:11 +0530

    warning fixed

--- src/main/java/com/vishal/microservices/restfulwebservices/VersioningPersonController/VersioningPersonController.java
+++ src/main/java/com/vishal/microservices/restfulwebservices/VersioningPersonController/VersioningPersonController.java
@@ -1,7 +1,6 @@
 package com.vishal.microservices.restfulwebservices.VersioningPersonController;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/vishal/microservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/vishal/microservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -3,7 +3,6 @@ package com.vishal.microservices.restfulwebservices.helloworld;
 import java.util.Locale;
 
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
